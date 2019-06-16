1ba21d98588443216f52de836dcf4fa9d17dac12
ivelthoven <ivovelthoven@hotmail.com>
2019-06-16 20:28:46 +0200

    dynamic port

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const restify = require('restify');
 
 // require("./db");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 
 const server = restify.createServer({
   name: 'api-ivelthoven',

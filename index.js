eaf99195506b4cefcca6e04cfe0bf30f8659595f
ivelthoven <ivo@ivelthoven.nl>
2019-08-18 20:57:12 +0200

    allow all to connect with CORS

--- index.js
+++ index.js
@@ -1,24 +1,23 @@
-const restify = require('restify');
-const corsMiddleware = require('restify-cors-middleware')
+const restify = require("restify");
+const corsMiddleware = require("restify-cors-middleware");
 const restifyLogger = require("restify-logger");
-
 
 // require("./db");
 
 const PORT = process.env.PORT || 5000;
- 
+
 const cors = corsMiddleware({
   preflightMaxAge: 5, //Optional
   origins: [
-    'https://projects.ivelthoven.nl', 
-    'https://ivelthoven.github.io'
-  ],
-})
-
+    "https://projects.ivelthoven.nl",
+    "https://ivelthoven.github.io",
+    "*"
+  ]
+});
 
 const server = restify.createServer({
-  name: 'api-ivelthoven',
-  version: '1.0.0'
+  name: "api-ivelthoven",
+  version: "1.0.0"
 });
 
 module.exports = server;
@@ -26,17 +25,17 @@ module.exports = server;
 server.pre(cors.preflight);
 server.use(cors.actual);
 
-server.use(restifyLogger("short"))
+server.use(restifyLogger("short"));
 
 // Index
 server.get("/", (req, res) => {
-    res.send("Welcome to IVelthoven API");
-})
+  res.send("Welcome to IVelthoven API");
+});
 
 // Github Routes
 require("./routes/github");
 
 // Start server
 server.listen(PORT, () => {
-  console.log('\x1b[34m%s\x1b[0m', `Ready on ${server.url}`);
+  console.log("\x1b[34m%s\x1b[0m", `Ready on ${server.url}`);
 });

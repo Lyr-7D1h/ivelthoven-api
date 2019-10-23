b8ad2eb198837c75938156268158660348c94d85
Ivo Velthoven <ivelthoven@protonmail.com>
2019-10-23 11:48:45 +0200

    updated cors

--- index.js
+++ index.js
@@ -10,9 +10,11 @@ const PORT = process.env.PORT || 5000
 const cors = corsMiddleware({
   preflightMaxAge: 5, // Optional
   origins: [
-    'https://projects.ivelthoven.nl',
-    'https://ivelthoven.github.io',
-    '*'
+    'https://*.ivelthoven.nl'
+    // 'https://projects.ivelthoven.nl',
+    // 'https://ivelthoven.github.io',
+    // 'https://blog.ivelthoven.nl',
+    // 'https://blog.ivelthoven.nl'
   ]
 })
 

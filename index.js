449a3a8948e2b221e0af080d4311ea666bf90575
Ivo Velthoven <ivelthoven@protonmail.com>
2019-10-26 16:13:30 +0200

    added github.io to cors

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ const PORT = process.env.PORT || 5000
 
 const cors = corsMiddleware({
   preflightMaxAge: 5, // Optional
-  origins: ['https://*.ivelthoven.nl']
+  origins: [
+    'https://projects.ivelthoven.nl',
+    'https://blog.ivelthoven.nl',
+    'https://ivelthoven.github.io'
+  ]
 })
 
 const server = restify.createServer({

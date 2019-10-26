22080a177f3acabba77a4f7d18efbc1ea0e311db
Ivo Velthoven <ivelthoven@protonmail.com>
2019-10-26 16:18:27 +0200

    removed path

--- routes/auth.js
+++ routes/auth.js
@@ -16,7 +16,7 @@ server.post('/auth', auth.basic, (req, res, next) => {
     .save()
     .then(() => {
       const token = `${key}:${secret.toString('hex')}`.toString('base64')
-      res.header('Set-Cookie', `admin=${token};path=/;`)
+      res.header('Set-Cookie', `admin=${token}`)
       res.send({ isValid: true })
     })
     .catch(err => {
